77c6fac3aea14d10a5e5be071dffe7b1613d5e13
Giao The Cung <giaothe@gmail.com>
2022-10-22 11:44:11 +0200

    Bump oauth2-klient

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
 
     implementation(Konfig.konfig)
     implementation(Kotlin.Logging.kotlinLogging)
-    implementation("com.github.navikt.dp-biblioteker:oauth2-klient:2022.05.30-09.37.623ee13a49dd")
+    implementation("com.github.navikt.dp-biblioteker:oauth2-klient:2022.10.22-09.05.6fcf3395aa4f")
     implementation(Ktor2.Client.library("logging"))
     implementation(Ktor2.Client.library("cio"))
     implementation(Ktor2.Client.library("content-negotiation"))
