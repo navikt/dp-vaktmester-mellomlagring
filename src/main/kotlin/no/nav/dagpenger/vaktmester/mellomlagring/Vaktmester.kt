13451fbb3532b5561a809059ec70dc8da23d334c
Geir André Lund <geir.andre.lund@nav.no>
2025-06-02 15:13:14 +0200

    Oppdatere java 21 og gradle 8.13. Tilpasser biblioteksendringer.

--- src/main/kotlin/no/nav/dagpenger/vaktmester/mellomlagring/Vaktmester.kt
+++ src/main/kotlin/no/nav/dagpenger/vaktmester/mellomlagring/Vaktmester.kt
@@ -1,11 +1,13 @@
 package no.nav.dagpenger.vaktmester.mellomlagring
 
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
+import com.github.navikt.tbd_libs.rapids_and_rivers.River
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
+import io.micrometer.core.instrument.MeterRegistry
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-import no.nav.helse.rapids_rivers.JsonMessage
-import no.nav.helse.rapids_rivers.MessageContext
-import no.nav.helse.rapids_rivers.RapidsConnection
-import no.nav.helse.rapids_rivers.River
 import java.util.UUID
 
 internal class Vaktmester(rapidsConnection: RapidsConnection, private val mellomlagringClient: MellomlagringClient) :
@@ -24,6 +26,8 @@ internal class Vaktmester(rapidsConnection: RapidsConnection, private val mellom
     override fun onPacket(
         packet: JsonMessage,
         context: MessageContext,
+        metadata: MessageMetadata,
+        meterRegistry: MeterRegistry,
     ) {
         val ident = packet.ident()
         val soknaId = packet.søknadUuid()
